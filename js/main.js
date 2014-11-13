16cb7f61f8548c46ac853cc285638e008120f830
codingcool21 <bryan.quadras2001@gmail.com>
2014-11-13 11:37:46 +0000

    fixed enter keyup and all elements centered when window is resized

--- js/main.js
+++ js/main.js
@@ -1,14 +1,27 @@
+// initialize an object which will contain all variables and functions
 var $scope = {};
+// run the function when the DOM is ready and thew page has loaded
 $(function () {
-    var remDatabase = new Firebase("https://updatemessage.firebaseio.com/");
-    remDatabase.child("message").on("value", function (snapshot) {
+    bootbox.alert("Hi there!");
+    
+    // new variable that will link to Firebase database
+    $scope.remDatabase = new Firebase("https://updatemessage.firebaseio.com/");
+    
+    // when somethings changed in the Firebase, instantly update the text display
+    $scope.remDatabase.child("message").on("value", function (snapshot) {
         $("#text_display").text(snapshot.val());
     });
+    
+    // sendData function to store data in Firebase and update all clients
     $scope.sendData = function (text_message) {
-        remDatabase.update({
+        // local function to update and send changes
+        // 
+        $scope.remDatabase.update({
             "message": text_message
         });
     }
+    
+    // used to center element on page horizontally
     $scope.centerElementOnPage = function (element, element_value, css_size_word) {
         if (css_size_word == "em") {
             var css_word = "em";
@@ -19,12 +32,19 @@ $(function () {
         $(element).css("position", "relative");
         $(element).css("left", $(window).innerWidth() / 2 - (element_value * 0.5) + css_word);
     }
-    $scope.centerElementOnPage($("#text_display"), 500, "px");
+    //$scope.centerElementOnPage($("#text_display"), 500, "px");
     $scope.centerElementOnPage($("#send_btn"), 65, "px");
     $scope.centerElementOnPage($("#textarea"), 300, "px");
     $("#textarea").keyup(function (e) {
         if (e.keyCode == 13) {
-            $scope.sendData($("#textarea").val());
+            var text = $("#textarea").val();
+            $scope.sendData(text);
+            $("#textarea").val("");
         }
+    });
+    $(window).resize(function() {
+        $scope.centerElementOnPage($("#send_btn"), 65, "px");
+        $scope.centerElementOnPage($("#textarea"), 300, "px");
+        $("#textarea").val($(window).innerWidth());
     });
 });
