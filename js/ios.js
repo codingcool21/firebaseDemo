9ace2de4caf555c6dbf61db99c984e36dc213472
codingcool21 <bryan.quadras2001@gmail.com>
2014-12-21 20:04:17 +0400

    Added code to make sure elements resize while window size changes

--- js/ios.js
+++ js/ios.js
@@ -70,7 +70,11 @@ $scope.setButtonAction = function(button_object_string, function_to_call) {
     $(button_object_string).click(function_to_call);
 }
 $(function () {
+    function centerElements () {
     $scope.centerElementOnPage("[data-targetn='login']", 550, "px", "left");
     $scope.centerElementOnPage("[data-targetn='login']", 300 + 185, "px", "top");
     $scope.setButtonAction($("#log_in_btn"), function() {$("[data-targetn='login']").show()});
+    }
+    centerElements()
+    $(window).resize(centerElements);
 });
