183066528778a0a6293547cd663f9f37d2149a95
codingcool21 <bryan.quadras2001@gmail.com>
2014-12-22 19:41:18 +0400

    Added text/video display from Firebase - controller and viewer added

--- js/ios.js
+++ js/ios.js
@@ -33,7 +33,8 @@ $scope.authWithLogin = function() {
                                     //alert(error);
                                     if(!error) {
                                         //alert("yes it works...");
-                                        $scope.dialogUI.close();
+                                        //$scope.dialogUI.close();
+                                        $scope.closeLoginDialog();
                                         location.reload();
                                     }
                                 });
@@ -67,6 +68,79 @@ $scope.setButtonAction = function(button_object_string, function_to_call) {
     $(button_object_string).click(function_to_call);
 }
 $(function() {
+    localStorage.setItem("auth", "false");
+    $scope.yoyo = "";
+    $scope.find_out_param_url == "";
+    $scope.find_out_param_url = function (key_param, callback) {
+        var search_param = location.search;
+        if (search_param.indexOf(key_param) == -1) {
+            if (callback == undefined) {
+                return false;
+            } else {
+                callback(false);
+            }
+        }
+        search_param = search_param.split(key_param + "=");
+        yoyo = search_param[0];
+        var search_param_tmp = search_param[1];
+        if (search_param_tmp == undefined) {
+            if (callback == undefined) {
+                return false;
+            } else {
+                callback(false);
+            }
+        } else {
+            var search_param_tmp_boyo = search_param_tmp.split("&");
+            //search_param_tmp = search_param_tmp_boyo[1];
+            if (callback == undefined) {
+                return decodeURI(search_param_tmp_boyo[0]);
+            } else {
+                callback(decodeURI(search_param_tmp_boyo[0]));
+            }
+        }
+    };
+    $scope.groupParam = $scope.find_out_param_url("groupname");
+    if ($scope.groupParam == false) {
+        $scope.groupParam = "message";
+    }
+
+    //bootbox.alert("Hi there!");
+
+
+
+    // when somethings changed in the Firebase, instantly update the text display
+    $scope.remDatabase.child($scope.groupParam).on("value", function (snapshot) {
+        $("#textdisplay").html("&nbsp;");
+        localStorage.setItem("auth", "true");
+        switch(snapshot.val().type) {
+            case "message": $("#textdisplay").text(snapshot.val().value);
+                break;
+            case "youtube": $("#textdisplay").html("<iframe width='560' height='315' src='//www.youtube.com/embed/" + snapshot.val().value +"' frameborder='0' allowfullscreen></iframe>");
+        }
+
+    }, function (error) {
+        //alert(error);
+        //
+        $("[data-targetn='login']").show()
+            $scope.dialogIsHiddenOrShown = 0;
+            $("#overlay").show();
+
+    });
+    //$scope.checkIfAuthIsLocal = function () {
+    // if ($("[data-popup = 'true'") == "") {
+    //    return;
+    // } else {
+    //     if (localStorage.getItem("auth") == "false") {
+    $("[data-popup = 'true']").css("display", "block").appendTo("#ui-dialog");
+    //          $scope.dialogUI.create()
+    //   }
+    //  }
+    //  }
+
+    //setTimeout(function () {
+    //    $scope.checkIfAuthIsLocal()
+  //  }, 1300);
+
     $scope.centerElements = function() {
         //alert("Page width is " + $(window).innerWidth() + " and height is " + $(window).innerHeight());
         $scope.centerElementOnPage("#statusBar", $("#statusBar").width() + 150, "px", "left");
